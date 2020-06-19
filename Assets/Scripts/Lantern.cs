7264dde4b41a26c53c0f8dc1080fd210614793c9
Sherwesh Bajracharya <sbajracharya@fortuitycreative.com>
2020-06-19 21:26:32 +1000

    176. Setup intial raycasting.

---
+++ Assets/Scripts/Lantern.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lantern : MonoBehaviour
+{
+    [SerializeField] Camera playerCamera;
+    [SerializeField] float range = 10f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        var rayCast = Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, range);
+        if (rayCast)
+        {
+            print(hit.collider.name);
+        }
+    }
+}
