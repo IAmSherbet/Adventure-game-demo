439ccc680d4e24e0292c8b7d52c3e37d81c6df18
Sherwesh Bajracharya <sbajracharya@fortuitycreative.com>
2020-06-30 08:25:46 +1000

    192. Added weapon zoom by updating field of view

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 30f;
+
+    bool zoomedInToggle = false;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if (zoomedInToggle == false)
+            {
+                zoomedInToggle = true;
+                fpsCamera.fieldOfView = zoomedInFOV;
+            }
+            else
+            {
+                zoomedInToggle = false;
+                fpsCamera.fieldOfView = zoomedOutFOV;
+            }
+        }  
+    }
+}
