bec22dc75f26a52cc8ca566b265ec02be1880b5a
Sherwesh Bajracharya <sbajracharya@fortuitycreative.com>
2020-07-09 17:09:13 +1000

    me - refacrtored Ragdoll and death events code into new EnemyDeath script.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,41 +7,23 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] float chaseRange = 5f;
     [SerializeField] float turnSpeed = 5f;
-    [SerializeField] float dyingBlowForce = 1000f;
 
     Transform targetToChase;
-
-    public List<Collider> ragdollParts = new List<Collider>();
-    public Rigidbody rigidBody;
-    public float speed;
-    public float angularSpeed;
-
     NavMeshAgent navMeshAgent;
+
     float distanceToTarget = Mathf.Infinity;
     bool isProvoked = false;
-
-    private void Awake()
-    {
-        SetRagdollParts();
-    }
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         targetToChase = FindObjectOfType<PlayerHealth>().gameObject.transform;
-        rigidBody = GetComponent<Rigidbody>();
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, chaseRange);
-    }
-
-    private void FixedUpdate()
-    {
-        speed = rigidBody.velocity.magnitude;
-        angularSpeed = rigidBody.angularVelocity.magnitude;
     }
 
     void Update()
@@ -56,45 +38,6 @@ public class EnemyAI : MonoBehaviour
         {
             isProvoked = true;
         }
-    }
-
-    private void SetRagdollParts()
-    {
-        Collider[] colliders = gameObject.GetComponentsInChildren<Collider>();
-
-        foreach (Collider c in colliders)
-        {
-            if (c.gameObject != gameObject)
-            {
-                c.isTrigger = true;
-                ragdollParts.Add(c);
-            }
-        }
-    }
-
-    public void ActivateRagdoll()
-    {
-        rigidBody.velocity = Vector3.zero;
-        rigidBody.angularVelocity = Vector3.zero;
-        rigidBody.useGravity = false;
-
-        gameObject.GetComponent<CapsuleCollider>().enabled = false;
-        gameObject.GetComponent<Animator>().enabled = false;
-        gameObject.GetComponent<Animator>().avatar = null;
-
-        foreach (Collider c in ragdollParts)
-        {
-            if (c.gameObject != gameObject)
-            {
-                c.isTrigger = false;
-                c.attachedRigidbody.velocity = Vector3.zero;
-                c.attachedRigidbody.angularVelocity = Vector3.zero;
-                c.attachedRigidbody.AddForce(Vector3.back * dyingBlowForce);
-            }
-        }
-
-        gameObject.GetComponent<EnemyAI>().enabled = false;
-        gameObject.GetComponent<NavMeshAgent>().enabled = false;
     }
 
     public void OnDamageTaken()
---
+++ Assets/Scripts/EnemyDeath.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyDeath : MonoBehaviour
+{
+    [SerializeField] float dyingBlowForce = 1000f;
+
+    public Rigidbody rigidBody;
+    public EnemyAI enemyAI;
+    public NavMeshAgent navMeshAgent;
+    public List<Collider> ragdollParts = new List<Collider>();
+
+    private void Awake()
+    {
+        SetRagdollParts();
+    }
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        enemyAI = GetComponent<EnemyAI>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void SetRagdollParts()
+    {
+        Collider[] colliders = gameObject.GetComponentsInChildren<Collider>();
+
+        foreach (Collider c in colliders)
+        {
+            if (c.gameObject != gameObject)
+            {
+                c.isTrigger = true;
+                ragdollParts.Add(c);
+            }
+        }
+    }
+
+    public void ActivateRagdoll()
+    {
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.angularVelocity = Vector3.zero;
+        rigidBody.useGravity = false;
+
+        gameObject.GetComponent<CapsuleCollider>().enabled = false;
+        gameObject.GetComponent<Animator>().enabled = false;
+        gameObject.GetComponent<Animator>().avatar = null;
+
+        foreach (Collider c in ragdollParts)
+        {
+            if (c.gameObject != gameObject)
+            {
+                c.isTrigger = false;
+                c.attachedRigidbody.velocity = Vector3.zero;
+                c.attachedRigidbody.angularVelocity = Vector3.zero;
+                c.attachedRigidbody.AddForce(Vector3.back * dyingBlowForce);
+            }
+        }
+
+        enemyAI.enabled = false;
+        navMeshAgent.enabled = false;
+    }
+}
