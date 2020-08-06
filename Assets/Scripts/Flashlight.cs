ba5050279b10e6b351d0a0f74bcfb832231839f0
Sherwesh Bajracharya <sbajracharya@fortuitycreative.com>
2020-08-07 08:00:36 +1000

    213. Added Flashlight. Tried using decreasing intensity and angle like course.

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    [SerializeField] float lightStartIntensity = 4f;
+    [SerializeField] float lightStartAngle = 80f;
+
+    [SerializeField] float lightDecay = 0.1f;
+    [SerializeField] float angleDecay = 1f;
+
+    [SerializeField] float minimumAngle = 40f;
+    [SerializeField] float lightLifetime = 60f;
+    float timeBetweenDecay;
+    Light myLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeBetweenDecay = lightLifetime / (lightStartIntensity / lightDecay);
+
+        myLight = GetComponent<Light>();
+        myLight.intensity = lightStartIntensity;
+        myLight.spotAngle = lightStartAngle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if light is ON
+        //StartCoroutine(DecreaseLightAngle());
+        StartCoroutine(DecreaseLightIntensity());
+        //DecreaseLightIntensity();
+
+        // else do nothing
+    }
+
+    IEnumerator DecreaseLightAngle()
+    {
+        if (myLight.spotAngle >= minimumAngle)
+        {
+            myLight.spotAngle -= angleDecay;
+            yield return new WaitForSeconds(timeBetweenDecay);
+        }
+    }
+
+    IEnumerator DecreaseLightIntensity()
+    {
+        myLight.intensity -= lightDecay;
+        yield return new WaitForSeconds(10f);
+    }
+}
